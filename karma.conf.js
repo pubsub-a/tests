7f176ec6d8cc9909317c1f84183017323231d34f
Timo Dörr <timo@latecrew.de>
2017-11-18 12:26:18 +0100

    Fix chrome headless args

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,14 @@ module.exports = function (config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     // browsers: ['Chrome', 'Firefox'],
-    browsers: ['ChromeHeadless'],
+    browsers: ['MyChromeHeadless'],
+
+    customLaunchers: {
+      MyChromeHeadless: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions', '--no-sandbox', '--disable-gpu']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
