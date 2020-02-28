af52c8a884e459a7229155e481d019d8864a8ebd
Timo Dörr <timo@latecrew.de>
2020-02-28 14:31:04 +0100

    More error handling tests

--- tests/spec/test_errorhandling.ts
+++ tests/spec/test_errorhandling.ts
@@ -1,4 +1,5 @@
 import { ImplementationFactory, PubSub } from "@pubsub-a/interfaces";
+import { expect } from "chai";
 
 export const executeErrorHandlingTests = (factory: ImplementationFactory) => {
     let pubsub: PubSub;
@@ -30,7 +31,35 @@ export const executeErrorHandlingTests = (factory: ImplementationFactory) => {
             });
         });
 
-        it.skip("should trigger the promise catch when the control message is malformed", done => {
+        it("should trigger the promise catch when the control message is unknown for CONTROL_SUBSCRIBE", done => {
+            pubsub.channel("__control").then(controlChannel => {
+                controlChannel
+                    .publish("CONTROL_SUBSCRIBE", {
+                        type: "INVALID"
+                    } as any)
+                    .catch(err => {
+                        expect(err.indexOf("invalid")).to.be.greaterThan(-1);
+                        expect(err.indexOf("CONTROL_SUBSCRIBE")).to.be.greaterThan(-1);
+                        done();
+                    });
+            });
+        });
+
+        it("should trigger the promise catch when the control message is unknown for CONTROL_UNSUBSCRIBE", done => {
+            pubsub.channel("__control").then(controlChannel => {
+                controlChannel
+                    .publish("CONTROL_UNSUBSCRIBE", {
+                        type: "INVALID"
+                    } as any)
+                    .catch(err => {
+                        expect(err.indexOf("invalid")).to.be.greaterThan(-1);
+                        expect(err.indexOf("CONTROL_UNSUBSCRIBE")).to.be.greaterThan(-1);
+                        done();
+                    });
+            });
+        });
+
+        it("should trigger the promise catch when the control message to SUBSCRIBE_DISCONNECT is malformed", done => {
             pubsub.channel("__control").then(controlChannel => {
                 controlChannel
                     .publish("CONTROL_SUBSCRIBE", {
@@ -38,6 +67,25 @@ export const executeErrorHandlingTests = (factory: ImplementationFactory) => {
                         // clientId is missing => malformed
                     } as any)
                     .catch(err => {
+                        console.info(err);
+                        expect(err.indexOf("empty")).to.be.greaterThan(0);
+                        expect(err.indexOf("clientUuid")).to.be.greaterThan(0);
+                        done();
+                    });
+            });
+        });
+
+        it.skip("should trigger the promise catch when the control message to UNSUBSCRIBE_DISCONNECT is malformed", done => {
+            pubsub.channel("__control").then(controlChannel => {
+                controlChannel
+                    .publish("CONTROL_UNSUBSCRIBE", {
+                        type: "UNSUBSCRIBE_DISCONNECT"
+                        // clientId is missing => malformed
+                    } as any)
+                    .catch(err => {
+                        console.info(err);
+                        expect(err.indexOf("empty")).to.be.greaterThan(0);
+                        expect(err.indexOf("clientUuid")).to.be.greaterThan(0);
                         done();
                     });
             });
